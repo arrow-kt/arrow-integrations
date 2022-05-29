6ef9db67336ff4e0bd89433a9708af9b67c82295
Mitchell Yuwono <myuwono@atlassian.com>
2022-05-29 10:00:18 +1000

    use arrow-gradle-config 0.10.3-alpha.3

--- arrow-integrations-jackson-module/build.gradle.kts
+++ arrow-integrations-jackson-module/build.gradle.kts
@@ -8,12 +8,6 @@ plugins {
   `java-library`
 }
 
-java {
-  toolchain {
-    languageVersion.set(JavaLanguageVersion.of(8))
-  }
-}
-
 animalsniffer {
   sourceSets = sourceSets.find { it.name == "main" }?.let(::listOf).orEmpty() // Ignore tests
   ignore = listOf("java.lang.*")
