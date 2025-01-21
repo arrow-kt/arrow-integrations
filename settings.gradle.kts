af7d2f079f7e0d33726699575dabc3bc02f626d0
Alejandro Serrano <trupill@gmail.com>
2025-01-21 14:08:49 +0100

    Run build on Mac

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,11 @@ pluginManagement {
   }
 }
 
+plugins {
+  id("com.gradle.develocity") version "3.19"
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+}
+
 dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
